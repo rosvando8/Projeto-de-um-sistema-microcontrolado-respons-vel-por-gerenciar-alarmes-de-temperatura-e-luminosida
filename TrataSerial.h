b08c13e8c80976fc7ed11ab91a7de5fccc9d4ad7
Rosvando Marques Gonzaga Júnior <rosvandojunior@hotmail.com>
2017-07-12 16:03:13 -0300

    Create TrataSerial.h

---
+++ TrataSerial.h
@@ -0,0 +1,12 @@
+#ifndef TRATASERIAL_H
+        #define TRATASERIAL_H
+
+#include <LPC11xx.h>			/* LPC11xx Peripheral Registers */
+#include "system_LPC11xx.h"
+#include "adc.h"
+#include "serial.h"
+
+char checaSerial(void);
+void ProcessarMens(void);
+
+#endif
