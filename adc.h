8632437d128f2cce37bf01c52db90a91265441aa
Rosvando Marques Gonzaga Júnior <rosvandojunior@hotmail.com>
2017-07-12 16:18:05 -0300

    Create adc.h

---
+++ adc.h
@@ -0,0 +1,10 @@
+#ifndef ADC_H
+        #define ADC_H
+
+#include <LPC11xx.h>			/* LPC11xx Peripheral Registers */
+#include "system_LPC11xx.h"
+
+
+    void InicializaAD();
+    int LeAD();
+#endif
