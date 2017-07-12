6b081b81508c886dae24c92a8e6a93cdbe8f851f
Rosvando Marques Gonzaga Júnior <rosvandojunior@hotmail.com>
2017-07-12 16:16:49 -0300

    Create alarme.h

---
+++ alarme.h
@@ -0,0 +1,10 @@
+#ifndef ALARME_H
+        #define ALARME_H
+
+#include <LPC11xx.h>			/* LPC11xx Peripheral Registers */
+#include "system_LPC11xx.h"
+#include "adc.h"
+
+unsigned int checaAlarme(void);
+
+#endif
